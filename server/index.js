6626e4cd11d483cd4cc4425f125912f44ba8129b
Avi <30874943+saviral425@users.noreply.github.com>
2020-11-01 09:26:22 -0800

    uploading my forst code on repo

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('./public'));
+app.get('/url/:id', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    });
+});
+
+app.get('/:id', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    });
+});
+
+app.post('/url', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    });
+});
+
+const port = process.env.PORT || 6969;
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
